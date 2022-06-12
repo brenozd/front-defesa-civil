5906521bef57b51ab54346dae72eb2e7feae2265
Breno Zanato Detomini <breno.detomini@beegol.com>
2022-06-12 10:38:33 -0300

    Refactoring profile page Fixing location swap

--- app/lib/pages/my_profile_page.dart
+++ app/lib/pages/my_profile_page.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:app/common.dart';
 import 'package:app/services/location_service.dart';
@@ -32,6 +31,94 @@ class _MyProfilePageState extends State<MyProfilePage> {
     if (useLocation) {
       profilePostalCode = LocationService().getPostalCode();
     }
+
+    var postalCodeFormField = TextFormField(
+      readOnly: useLocation,
+      obscureText: false,
+      controller: TextEditingController(text: profilePostalCode),
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          helperText: 'Postal Code',
+          enabled: true),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Postal Code cannot be empty";
+        }
+        if (!LocationService()
+            .setCurrentLocation(profilePostalCode, profileCountryCode)) {
+          return "Invalid postal code format for you region";
+        }
+        return null;
+      },
+      onFieldSubmitted: (value) {
+        setState(() {
+          profilePostalCode = value;
+        });
+        _formKey.currentState!.validate();
+      },
+    );
+
+    var countryCodeDropDown = Visibility(
+        visible: !useLocation,
+        child: DropdownButtonFormField<String>(
+            value: profileCountryCode,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: "Country Code"),
+            icon: const Icon(Icons.arrow_drop_down_rounded),
+            items: LocationService()
+                .getCountryCodeRegexMap()
+                .keys
+                .map((String value) {
+              return DropdownMenuItem<String>(value: value, child: Text(value));
+            }).toList(),
+            onChanged: (String? value) {
+              setState(() {
+                profileCountryCode = value!;
+              });
+              _formKey.currentState!.validate();
+            }));
+
+    var useLocationCheckBox = Align(
+        alignment: Alignment.center,
+        child: CheckboxListTile(
+            title: const Text("Use location"),
+            value: useLocation,
+            onChanged: (bool? value) => setState(() {
+                  useLocation = value!;
+                  LocationService().enableBackground = value;
+                  LocationService().update().then((success) =>
+                      profilePostalCode = LocationService().getPostalCode());
+                })));
+
+    var warningLevelDropDown = DropdownButtonFormField<String>(
+        value: currentWarningLevel,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(), labelText: "Warning Level"),
+        icon: const Icon(Icons.arrow_drop_down_rounded),
+        items: dropDownItens.map((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        onChanged: (String? value) => setState(() {
+              currentWarningLevel = value!;
+            }));
+
+    var warningTypes = Column(
+      children: warningItens.keys.map((String key) {
+        return CheckboxListTile(
+          title: Text(key),
+          value: warningItens[key],
+          onChanged: (bool? value) {
+            setState(() {
+              warningItens[key] = value!;
+            });
+          },
+        );
+      }).toList(),
+    );
+
     return Form(
         key: _formKey,
         child: Scaffold(
@@ -42,113 +129,17 @@ class _MyProfilePageState extends State<MyProfilePage> {
             child: Column(
               children: [
                 Separator(text: "Location"),
-                TextFormField(
-                  readOnly: useLocation,
-                  obscureText: false,
-                  controller: TextEditingController(text: profilePostalCode),
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      helperText: 'Postal Code',
-                      enabled: true),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Postal Code cannot be empty";
-                    }
-                    RegExp validatePostalCode =
-                        RegExp(LocationService().getPostalCodeRegExp());
-                    // limita a avisos somente da pais da localização atual
-                    if (validatePostalCode.hasMatch(value) == false) {
-                      return "Invalid postal code format for your location";
-                    }
-                    return null;
-                  },
-                  onFieldSubmitted: (value) {
-                    profilePostalCode = value;
-                    _formKey.currentState!.validate();
-                    LocationService().setCurrentLocation(
-                        profilePostalCode, profileCountryCode);
-                  },
-                ),
-                Visibility(
-                  child: SizedBox(
-                    height: 15,
-                  ),
-                  visible: !useLocation,
-                ),
-                Visibility(
-                  child: DropdownButtonFormField<String>(
-                      value: profileCountryCode,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Country Code"),
-                      icon: const Icon(Icons.arrow_drop_down_rounded),
-                      items: LocationService()
-                          .countryCodeRegexMap
-                          .keys
-                          .map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? value) {
-                        setState(() {
-                          profileCountryCode = value!;
-                        });
-                        _formKey.currentState!.validate();
-                        LocationService().setCurrentLocation(
-                            profilePostalCode, profileCountryCode);
-                      }),
-                  visible: !useLocation,
-                ),
-                Align(
-                    alignment: Alignment.center,
-                    child: CheckboxListTile(
-                        title: const Text("Use location"),
-                        value: useLocation,
-                        onChanged: (bool? value) => setState(() {
-                              useLocation = value!;
-                              LocationService().enableBackground = value;
-                              profilePostalCode =
-                                  LocationService().getPostalCode();
-                            }))),
+                postalCodeFormField,
+                Visibility(child: SizedBox(height: 15), visible: !useLocation),
+                countryCodeDropDown,
+                useLocationCheckBox,
                 Separator(text: "Notifications"),
-                // TODO: Instead of dropdown, maybe checkboxes?
-                DropdownButtonFormField<String>(
-                    value: currentWarningLevel,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Warning Level"),
-                    icon: const Icon(Icons.arrow_drop_down_rounded),
-                    items: dropDownItens.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) => setState(() {
-                          currentWarningLevel = value!;
-                        })),
-                SizedBox(
-                  height: 15,
-                ),
+                warningLevelDropDown,
+                SizedBox(height: 15),
                 Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text("Notificate me about"),
-                ),
-                Column(
-                  children: warningItens.keys.map((String key) {
-                    return CheckboxListTile(
-                      title: Text(key),
-                      value: warningItens[key],
-                      onChanged: (bool? value) {
-                        setState(() {
-                          warningItens[key] = value!;
-                        });
-                      },
-                    );
-                  }).toList(),
-                )
+                    alignment: Alignment.centerLeft,
+                    child: Text("Notificate me about")),
+                warningTypes
               ],
             ),
           )),
